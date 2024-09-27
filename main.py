35a71d0f3dbcb2a024bd6e0fb5635d4f2c538417
mrakobeska <polrus972@gmail.com>
2024-09-27 15:53:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    string_ = str(string)
+    result = (len(string_), string_.upper(), string_.lower())
+    count_calls()
+    return result
+
+def is_contains(string, list_to_search):
+    string = str(string).upper()
+    list_ = list(list_to_search)
+    count_calls()
+    for i in range(len(list_)):
+        if string == str(list_[i].upper()):
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
+
